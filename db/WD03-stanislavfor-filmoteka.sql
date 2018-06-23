1503c2e6c4b0956093435876f138ad75569c88e4
stanislavfor <stasposter@narod.ru>
2018-06-23 07:55:23 +0300

    data base ready commit

---
+++ db/WD03-stanislavfor-filmoteka.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 23 2018 г., 07:53
+-- Версия сервера: 5.6.38
+-- Версия PHP: 5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `filmoteka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `films`
+--
+
+CREATE TABLE `films` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `genre` text NOT NULL,
+  `year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `films`
+--
+
+INSERT INTO `films` (`id`, `title`, `genre`, `year`) VALUES
+(11, 'Такси 2', 'комедия', 2000),
+(13, 'Облачный атлас', 'драма', 2012),
+(14, 'Чёрная пантера', 'фантастика, боевик', 2018);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `films`
+--
+ALTER TABLE `films`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `films`
+--
+ALTER TABLE `films`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
